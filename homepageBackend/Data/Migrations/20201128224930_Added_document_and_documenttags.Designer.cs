190f393819fe517fa47973d2b63dd5978c92388c
unknown <thom.florian.1997@googlemail.com>
2020-11-28 23:50:18 +0100

    After ef add and update

--- homepageBackend/Data/Migrations/20201128224930_Added_document_and_documenttags.Designer.cs
+++ homepageBackend/Data/Migrations/20201128224930_Added_document_and_documenttags.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using homepageBackend.Data;
@@ -9,9 +10,10 @@ using homepageBackend.Data;
 namespace homepageBackend.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201128224930_Added_document_and_documenttags")]
+    partial class Added_document_and_documenttags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ homepageBackend/Data/Migrations/20201128224930_Added_document_and_documenttags.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace homepageBackend.Data.Migrations
+{
+    public partial class Added_document_and_documenttags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTags_Projects_ProjectId",
+                table: "ProjectTags");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTags_Tags_TagName",
+                table: "ProjectTags");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectTags",
+                table: "ProjectTags");
+
+            migrationBuilder.RenameTable(
+                name: "ProjectTags",
+                newName: "ProjectTag");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProjectTags_TagName",
+                table: "ProjectTag",
+                newName: "IX_ProjectTag_TagName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectTag",
+                table: "ProjectTag",
+                columns: new[] { "ProjectId", "TagName" });
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Topic = table.Column<string>(type: "text", nullable: true),
+                    Link = table.Column<string>(type: "text", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorId = table.Column<string>(type: "text", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdaterId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documents_AspNetUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Documents_AspNetUsers_UpdaterId",
+                        column: x => x.UpdaterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentTag",
+                columns: table => new
+                {
+                    TagName = table.Column<string>(type: "text", nullable: false),
+                    DocumentId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTag", x => new { x.DocumentId, x.TagName });
+                    table.ForeignKey(
+                        name: "FK_DocumentTag_Documents_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "Documents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DocumentTag_Tags_TagName",
+                        column: x => x.TagName,
+                        principalTable: "Tags",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_CreatorId",
+                table: "Documents",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_UpdaterId",
+                table: "Documents",
+                column: "UpdaterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTag_TagName",
+                table: "DocumentTag",
+                column: "TagName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Projects_ProjectId",
+                table: "ProjectTag",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Tags_TagName",
+                table: "ProjectTag",
+                column: "TagName",
+                principalTable: "Tags",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Projects_ProjectId",
+                table: "ProjectTag");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Tags_TagName",
+                table: "ProjectTag");
+
+            migrationBuilder.DropTable(
+                name: "DocumentTag");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectTag",
+                table: "ProjectTag");
+
+            migrationBuilder.RenameTable(
+                name: "ProjectTag",
+                newName: "ProjectTags");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProjectTag_TagName",
+                table: "ProjectTags",
+                newName: "IX_ProjectTags_TagName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectTags",
+                table: "ProjectTags",
+                columns: new[] { "ProjectId", "TagName" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTags_Projects_ProjectId",
+                table: "ProjectTags",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTags_Tags_TagName",
+                table: "ProjectTags",
+                column: "TagName",
+                principalTable: "Tags",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
