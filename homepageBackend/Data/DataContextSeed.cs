f7ed835685a6fbe6add19ba2bdb5b5824485c2d8
unknown <thom.florian.1997@googlemail.com>
2020-12-14 00:09:55 +0100

    Added secret managment

--- homepageBackend/Data/DataContextSeed.cs
+++ homepageBackend/Data/DataContextSeed.cs
@@ -1,28 +1,36 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using homepageBackend.Domain;
+using homepageBackend.Options;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace homepageBackend.Data
 {
     public static class DataContextSeed
     {
         public static async Task SeedDefaultUserAsync(UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager, IConfiguration config)
         {
-            var administratorRole = new IdentityRole("Administrator");
+            var seedAdminProfile = new SeedAdminProfile();
+            config.Bind(nameof(seedAdminProfile), seedAdminProfile);            
+            
+            var administratorRole = new IdentityRole(seedAdminProfile.IdentityRoleName);
 
             if (roleManager.Roles.All(r => r.Name != administratorRole.Name))
             {
                 await roleManager.CreateAsync(administratorRole);
             }
 
-            var administrator = new ApplicationUser
-                {UserName = "administrator@localhost", Email = "administrator@localhost"};
-
-            if (userManager.Users.All(u => u.UserName != administrator.UserName))
+            var administrator = new ApplicationUser()
             {
-                await userManager.CreateAsync(administrator, "Administrator1!");
+                Email = seedAdminProfile.Email,
+                UserName = seedAdminProfile.Email
+            };
+
+            if (userManager.Users.All(u => u.Email != administrator.Email))
+            {
+                await userManager.CreateAsync(administrator, seedAdminProfile.Password);
                 await userManager.AddToRolesAsync(administrator, new[] {administratorRole.Name});
             }
         }
---
+++ homepageBackend/Options/SeedAdminProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace homepageBackend.Options
+{
+    public class SeedAdminProfile
+    {
+        public string IdentityRoleName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
